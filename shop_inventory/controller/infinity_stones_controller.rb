591f2e450afa3150cbe6fd5c65cb7a8800f9412d
Elliot Dixon <elliotjohndixon@gmail.com>
2019-10-08 04:07:53 +0100

    added new for infinity stones to views

--- shop_inventory/controller/infinity_stones_controller.rb
+++ shop_inventory/controller/infinity_stones_controller.rb
@@ -7,3 +7,13 @@ get '/infinity-stones' do
   @infinity_stones = InfintyStone.all()
   erb(:"infinity_stones/index")
 end
+
+get '/infinity-stones/new' do
+  @manufacturers = Manufacturer.all
+  erb(:new)
+end
+
+post '/infinity-stones' do
+  InfintyStone.new(params).save
+  redirect to '/infinity-stones'
+end

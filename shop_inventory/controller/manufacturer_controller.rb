dc6185412c3c19f21bf0d12349f7c0c66e83ee8e
Elliot Dixon <elliotjohndixon@gmail.com>
2019-10-08 09:34:01 +0100

    added index to manufacturer controller

--- shop_inventory/controller/manufacturer_controller.rb
+++ shop_inventory/controller/manufacturer_controller.rb
@@ -3,3 +3,8 @@ require('sinatra/contrib/all')
 require_relative('../models/infinity_stone.rb')
 require_relative('../models/manufacturer.rb')
 also_reload('../models/*')
+
+get '/manufacturers' do
+  @manufacturers = Manufacturer.all()
+  erb(:"manufacturers/index")
+end
